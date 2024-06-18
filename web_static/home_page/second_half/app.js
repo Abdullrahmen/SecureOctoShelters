7da4a940aa9c76305556850a87fb7b7b24eb150e
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-06-18 16:40:50 +0300

    Add the features octopus

--- web_static/home_page/second_half/app.js
+++ web_static/home_page/second_half/app.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+// feature_container animation on scrolling
+// Create the observer like the examples above
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animation');
+			return;
+		}
+		entry.target.classList.remove('animation');
+	});
+});
+
+// Get multiple elements instead of a single one using "querySelectorAll"
+features = document.querySelectorAll('.feature_container');
+features.forEach((element) => observer.observe(element));
+features = document.querySelectorAll('.title_1');
+features.forEach((element) => observer.observe(element));
+observer.observe(document.querySelector('.features_octopus'));
