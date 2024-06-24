3a0c1ea7067521e5b131e059635c76c382df474a
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-06-24 19:06:49 +0300

    initial latest news section

--- web_static/home_page/second_half/app.js
+++ web_static/home_page/second_half/app.js
@@ -16,3 +16,15 @@ features.forEach((element) => observer.observe(element));
 features = document.querySelectorAll('.title_1');
 features.forEach((element) => observer.observe(element));
 observer.observe(document.querySelector('.features_octopus'));
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function security_check_submit() {
+	document.getElementById("security_check_loading").style.display = "flex";
+	sleep(5000).then(() => { 
+		document.getElementById("security_check_loading").style.display = "none";
+		
+	 });
+}
